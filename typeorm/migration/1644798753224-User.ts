33631bcf54670a02f706a6e9ab23be4bacdb943f
Rodolfo Santana <rdmoedas@gmail.com>
2022-02-21 01:38:48 -0300

    style: prettiner in migration

--- typeorm/migration/1644798753224-User.ts
+++ typeorm/migration/1644798753224-User.ts
@@ -1,100 +1,125 @@
-import {MigrationInterface, QueryRunner, Table, TableForeignKey} from "typeorm";
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
 
 export class User1644798753224 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'persons',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            length: '250',
+            isNullable: false,
+          },
+          {
+            name: 'birthAt',
+            type: 'date',
+            isNullable: true,
+          },
+          {
+            name: 'phone',
+            type: 'varchar',
+            length: '16',
+            isNullable: true,
+          },
+          {
+            name: 'document',
+            type: 'varchar',
+            length: '14',
+            isNullable: true,
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'updatedAt',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+    );
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.createTable(new Table({
-            name: 'persons',
-            columns: [{
-                name: 'id',
-                type: 'int',
-                isPrimary: true,
-                isGenerated: true,
-                generationStrategy: 'increment',
-            }, {
-                name: 'name',
-                type: 'varchar',
-                length: '250',
-                isNullable: false
-            }, {
-                name: 'birthAt',
-                type: 'date',
-                isNullable: true
-            }, {
-                name: 'phone',
-                type: 'varchar',
-                length: '16',
-                isNullable: true
-            }, {
-                name: 'document',
-                type: 'varchar',
-                length: '14',
-                isNullable: true
-            }, {
-                name: 'createdAt',
-                type: 'timestamp',
-                default: 'CURRENT_TIMESTAMP',
-            }, {
-                name: 'updatedAt',
-                type: 'timestamp',
-                default: 'CURRENT_TIMESTAMP',
-            }]
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            length: '250',
+            isNullable: false,
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            length: '250',
+            isNullable: false,
+          },
+          {
+            name: 'photo',
+            type: 'varchar',
+            length: '250',
+            isNullable: true,
+          },
+          {
+            name: 'personId',
+            type: 'int',
+            isNullable: false,
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'updatedAt',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+    );
 
-        }));
-        
-        await queryRunner.createTable(new Table({
-            name: 'users',
-            columns: [{
-                name: 'id',
-                type: 'int',
-                isPrimary: true,
-                isGenerated: true,
-                generationStrategy: 'increment',
-            }, {
-                name: 'email',
-                type: 'varchar',
-                length: '250',
-                isNullable: false,
-                isUnique: true
-            }, {
-                name: 'password',
-                type: 'varchar',
-                length: '250',
-                isNullable: false
-            }, {
-                name: 'photo',
-                type: 'varchar',
-                length: '250',
-                isNullable: true
-            }, {
-                name: 'personId',
-                type: 'int',
-                isNullable: false,
-            }, {
-                name: 'createdAt',
-                type: 'timestamp',
-                default: 'CURRENT_TIMESTAMP',
-            }, {
-                name: 'updatedAt',
-                type: 'timestamp',
-                default: 'CURRENT_TIMESTAMP',
-            }]
-        }));
+    await queryRunner.createForeignKey(
+      'users',
+      new TableForeignKey({
+        columnNames: ['personId'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'persons',
+        name: 'FK_users_persons',
+        onDelete: 'CASCADE',
+      }),
+    );
+  }
 
-        await queryRunner.createForeignKey('users', new TableForeignKey({
-            columnNames: ['personId'],
-            referencedColumnNames: ['id'],
-            referencedTableName: 'persons',
-            name: 'FK_users_persons',
-            onDelete: 'CASCADE',
-        }))
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        // ! O down é na ordem inversa do up
-        await queryRunner.dropForeignKey('users', 'FK_users_persons');
-        await queryRunner.dropTable('users');
-        await queryRunner.dropTable('persons');
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    // ! O down é na ordem inversa do up
+    await queryRunner.dropForeignKey('users', 'FK_users_persons');
+    await queryRunner.dropTable('users');
+    await queryRunner.dropTable('persons');
+  }
 }
